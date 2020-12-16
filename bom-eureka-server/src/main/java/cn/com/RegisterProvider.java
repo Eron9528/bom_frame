a2de5418320bfc7e665313274c97ab55c7d29219
Eron9528 <1551447510@qq.com>
2020-12-17 01:49:49 +0800

    注册中心由于版本冲突一直出错，将boot 2.3.0 降低为2.1.3解决

---
+++ bom-eureka-server/src/main/java/cn/com/RegisterProvider.java
@@ -0,0 +1,15 @@
+package cn.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterProvider {
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterProvider.class, args);
+    }
+
+}

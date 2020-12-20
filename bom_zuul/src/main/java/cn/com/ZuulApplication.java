f440ec3211c5ce464710398b96804fb084c4e30b
Eron9528 <1551447510@qq.com>
2020-12-20 23:39:55 +0800

    加入zuul 网关

--- bom_zuul/src/main/java/cn/com/ZuulApplication.java
+++ bom_zuul/src/main/java/cn/com/ZuulApplication.java
@@ -1,16 +1,16 @@
 package cn.com;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableAutoConfiguration
-@SpringBootApplication
 @EnableEurekaClient
-public class Application {
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 
 }

b2cb3de6683a3e09145cec9c0a508611411a5add
Eron9527 <1187244022@qq.com>
2020-12-29 07:44:01 +0000

    增加网关

---
+++ bom-gateway/src/main/java/cn/com/GateWayApplciation.java
@@ -0,0 +1,24 @@
+package cn.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApplciation {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplciation.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/about")
+                        .uri("http://ityouknow.com"))
+                .build();
+    }
+}
--- bom-order/src/main/java/cn/com/taiji/service/OrderService.java
+++ bom-order/src/main/java/cn/com/taiji/service/OrderService.java
@@ -6,10 +6,35 @@ import java.util.List;
 
 public interface OrderService {
 
+    /**
+     * @描述   添加订单
+     * @创建人 Mayu
+     * @创建时间 2020/12/29
+    */
     void addOrder(String id);
+    /**
+     * @描述  删除订单
+     * @创建人 Mayu
+     * @创建时间 2020/12/29
+    */
     void deleteOrder(String id);
+    /**
+     * @描述  删除所有订单
+     * @创建人 Mayu
+     * @创建时间 2020/12/29
+    */
     void deleteAllOrder(List<String> ids);
+    /**
+     * @描述  更新订单
+     * @创建人 Mayu
+     * @创建时间 2020/12/29
+    */
     void update(Order order);
+    /**
+     * @描述  查询订单
+     * @创建人 Mayu
+     * @创建时间 2020/12/29
+    */
     void select(String id);
 
 }

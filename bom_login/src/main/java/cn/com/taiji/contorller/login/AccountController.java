531fbf4d567d1ef33634b83d21e9dd9dd8da3b28
Eron9528 <1551447510@qq.com>
2021-01-04 22:23:10 +0800

    配合vue login 请求

--- bom_login/src/main/java/cn/com/taiji/contorller/login/AccountController.java
+++ bom_login/src/main/java/cn/com/taiji/contorller/login/AccountController.java
@@ -1,11 +1,26 @@
 package cn.com.taiji.contorller.login;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.com.taiji.dto.Result;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/login/account")
+@RequestMapping("/api/account")
+@CrossOrigin
 public class AccountController {
+
+    @GetMapping("/test")
+    public Result login() {
+       return new Result("data");
+    }
+
+    @PostMapping("/login")
+    public Result login(@RequestParam String username,@RequestParam String password) {
+
+        if(username.equals("admin") && password.equals("123456"))
+            return new Result("success");
+        else
+            return new Result("error");
+    }
 
 
 

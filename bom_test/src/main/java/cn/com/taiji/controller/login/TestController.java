d1c7b7e4c738958bbad2fd7eb627f0d5049f970f
Eron9528 <1551447510@qq.com>
2021-01-25 00:21:25 +0800

    权限树动态绑定刷新菜单

--- bom_test/src/main/java/cn/com/taiji/controller/login/TestController.java
+++ bom_test/src/main/java/cn/com/taiji/controller/login/TestController.java
@@ -1,7 +1,9 @@
 package cn.com.taiji.controller.login;
 
+import cn.com.taiji.domain.user.Permission;
 import cn.com.taiji.domain.user.User;
 import cn.com.taiji.dto.Result;
+import cn.com.taiji.service.user.PermissionService;
 import cn.com.taiji.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +19,7 @@ import java.util.Map;
 public class TestController {
 
     @Autowired
-    private UserService userService;
+    PermissionService permissionService;
 
     @RequestMapping("login")
     public Map<String,String> login(@RequestParam("username") String username, @RequestParam("password") String password) {
@@ -34,71 +36,50 @@ public class TestController {
     @RequestMapping("getMenu")
     public Result getMenu(){
         Map<String, List<String[]>> menus = new HashMap<>();
-        // 用户管理
-        List<String[]> listUsers = new ArrayList<>();
-        listUsers.add(new String[]{"user", "用户列表"});
-        //权限管理
-        List<String[]> listRights = new ArrayList<>();
-        listRights.add(new String[]{"roles", "角色列表"});
-        listRights.add(new String[]{"rights", "权限列表"});
-        // 商品管理
-        List<String[]> listGoods = new ArrayList<>();
-        listGoods.add(new String[]{"goods", "商品列表"});
-        listGoods.add(new String[]{"addgoods","添加商品"});
-        // 订单管理
-        List<String[]> listOrder = new ArrayList<>();
-        listOrder.add(new String[]{"order","订单列表"});
-        listOrder.add(new String[]{"addorder","添加订单"});
+        // 先找到第一层菜单
+        List<Permission> firstLayer = permissionService.findFirstLayer();
+        firstLayer.forEach(first -> {
+            List<Permission> secondLayer = new ArrayList<>();
+            secondLayer = permissionService.findChildrenByMenuId(first.getId());
+            // 建立二级菜单容器
+            List<String[]> secondList = new ArrayList<>();
+            secondLayer.forEach(second -> {
 
-        menus.put("用户管理", listUsers);
-        menus.put("权限管理", listRights);
-        menus.put("商品管理", listGoods);
-        menus.put("订单管理",listOrder);
+                // 还有第三层的数据
+//                List<String[]> listfirst = new ArrayList<>();
+//                List<Permission> thirdLayer = new ArrayList<>();
+//                thirdLayer = permissionService.findChildrenByMenuId(first.getId());
+//                thirdLayer.forEach(third -> {
+//
+//                });
+                secondList.add(new String[]{second.getMenu(),second.getPermissionName()});
+            });
+            menus.put(first.getPermissionName(),secondList);
+        });
+
+
+        // 用户管理
+//        List<String[]> listUsers = new ArrayList<>();
+//        listUsers.add(new String[]{"user", "用户列表"});
+//        //权限管理
+//        List<String[]> listRights = new ArrayList<>();
+//        listRights.add(new String[]{"roles", "角色列表"});
+//        listRights.add(new String[]{"rights", "权限列表"});
+//        // 商品管理
+//        List<String[]> listGoods = new ArrayList<>();
+//        listGoods.add(new String[]{"goods", "商品列表"});
+//        listGoods.add(new String[]{"addgoods","添加商品"});
+//        // 订单管理
+//        List<String[]> listOrder = new ArrayList<>();
+//        listOrder.add(new String[]{"order","订单列表"});
+//        listOrder.add(new String[]{"addorder","添加订单"});
+//
+//        menus.put("用户管理", listUsers);
+//        menus.put("权限管理", listRights);
+//        menus.put("商品管理", listGoods);
+//        menus.put("订单管理",listOrder);
         return new Result(menus);
     }
 
-    @RequestMapping("users")
-    public Result getUser(String query , int pagenum, int pagesize){
-        List<User> users = userService.listUser();
-        return new Result(users);
-    }
 
-//    @PutMapping("users/{id}/")
-//    public Result setState(@PathVariable(value = "id")long id) {
-//        Account user = userService.changeStateById(id);
-//        return new Result(user);
-//    }
-
-    @PutMapping("users/{id}/{state}")
-    public Result setUserState(@PathVariable(value = "id")long id, @PathVariable("state") boolean state) {
-        User user = userService.changeStateById(id,state);
-        return new Result(user);
-    }
-
-    @PostMapping("adduser")
-    public Result addUser(@RequestBody User account) {
-        User user = userService.addUser(account);
-        return new Result(user);
-    }
-
-    @GetMapping("getUser/{id}")
-    public Result getUser(@PathVariable("id") long id){
-        User user = userService.getUserById(id);
-        return new Result(user);
-
-    }
-
-
-    @PutMapping("updateUser/{id}")
-    public Result updateUser(@RequestBody User account, @PathVariable("id") long id){
-        User user = userService.updateUserById(account, id);
-        return new Result(user);
-
-    }
-
-    @DeleteMapping("deleteUser/{id}")
-    public Result updateUser(@PathVariable("id") long id){
-        User user = userService.deleteById(id);
-        return new Result(user);
-    }
 }
--- bom_test/src/main/java/cn/com/taiji/controller/user/UserController.java
+++ bom_test/src/main/java/cn/com/taiji/controller/user/UserController.java
@@ -1,11 +1,63 @@
 package cn.com.taiji.controller.user;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.com.taiji.domain.user.User;
+import cn.com.taiji.dto.Result;
+import cn.com.taiji.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
 @CrossOrigin
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("users")
+    public Result getUser(String query , int pagenum, int pagesize){
+        List<User> users = userService.listUser();
+        return new Result(users);
+    }
+
+//    @PutMapping("users/{id}/")
+//    public Result setState(@PathVariable(value = "id")long id) {
+//        Account user = userService.changeStateById(id);
+//        return new Result(user);
+//    }
+
+    @PutMapping("users/{id}/{state}")
+    public Result setUserState(@PathVariable(value = "id")long id, @PathVariable("state") boolean state) {
+        User user = userService.changeStateById(id,state);
+        return new Result(user);
+    }
+
+    @PostMapping("adduser")
+    public Result addUser(@RequestBody User account) {
+        User user = userService.addUser(account);
+        return new Result(user);
+    }
+
+    @GetMapping("getUser/{id}")
+    public Result getUser(@PathVariable("id") long id){
+        User user = userService.getUserById(id);
+        return new Result(user);
+
+    }
+
+
+    @PutMapping("updateUser/{id}")
+    public Result updateUser(@RequestBody User account, @PathVariable("id") long id){
+        User user = userService.updateUserById(account, id);
+        return new Result(user);
+
+    }
+
+    @DeleteMapping("deleteUser/{id}")
+    public Result updateUser(@PathVariable("id") long id){
+        User user = userService.deleteById(id);
+        return new Result(user);
+    }
 }
